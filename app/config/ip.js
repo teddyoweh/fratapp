1b29af95569335c949530c5c097db1b59fca3f9b
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-09-11 14:40:13 -0500

    Update ip.js

--- app/config/ip.js
+++ app/config/ip.js
@@ -1,5 +1,5 @@
 const serverhost2 = '169.254.197.174'
-//const serverhost = '50.116.24.45'
-const serverhost = '172.20.10.10'//'50.116.24.45'/
+const serverhost_ = '50.116.24.45'
+const serverhost = serverhost_//'172.20.10.10'//'50.116.24.45'/
 const serverip = `http://${serverhost}:9990`
 export {serverip,serverhost}
