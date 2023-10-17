83ea9397c4198cf89eeedfd37880b6676151509a
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-10-16 22:01:48 -0500

    Update ip.js

--- app/config/ip.js
+++ app/config/ip.js
@@ -1,5 +1,5 @@
-const serverhost_ = "10.11.168.196"
-//const serverhost_ = '50.116.24.45'
+//const serverhost_ = "10.11.168.196"
+const serverhost_ = '50.116.24.45'
 const serverhost = serverhost_//'172.20.10.10'//'50.116.24.45'/
 //const serverhost_ = '50.116.24.45'
 const serverip = `http://${serverhost}:9990`
